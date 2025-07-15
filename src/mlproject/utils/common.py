77f41f71a560f6b0a4250b324dd91b1fb92fce39
rajat-cloudtechner <rajat.sharma@cloudtechner.com>
2025-07-15 15:38:40 +0530

    data ingestion added

--- src/mlproject/utils/common.py
+++ src/mlproject/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from mlproject import logger
+from mlproject.utils import logger
 import json
 import joblib
 from ensure import ensure_annotations
