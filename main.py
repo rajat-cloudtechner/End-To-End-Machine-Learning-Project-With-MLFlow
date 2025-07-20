ac4ea75d2901d1333194deedeb5d6934dbb8c241
rajat-cloudtechner <rajat.sharma@cloudtechner.com>
2025-07-20 15:45:17 +0530

    data validation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.mlproject.utils import logger
 from mlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlproject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 
@@ -7,6 +8,16 @@ STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
--- src/mlproject/config/configuration.py
+++ src/mlproject/config/configuration.py
@@ -1,6 +1,7 @@
 from mlproject.constants import *
 from mlproject.utils.common import read_yaml, create_directories
-from mlproject.entity.config_entity import DataIngestionConfig
+from mlproject.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
+
 class ConfigurationManager:
     def __init__(
         self,
@@ -29,3 +30,17 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            unzip_dir=config.unzip_data_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            all_schema=self.schema.COLUMNS
+        )
+
+        return data_validation_config
