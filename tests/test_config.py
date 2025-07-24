2ac62378ed48236c8ed36001cc89cbb05b74f535
rajat-cloudtechner <rajat.sharma@cloudtechner.com>
2025-07-24 11:26:00 +0530

    update

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,7 @@
 from src.mlproject.utils.common import read_yaml
+from pathlib import Path
+
 
 def test_read_yaml():
-    config = read_yaml("config/config.yaml")
+    config = read_yaml(Path("config/config.yaml"))
     assert isinstance(config, dict)
